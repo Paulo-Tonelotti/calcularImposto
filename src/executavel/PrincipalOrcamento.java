a59bdd0e617683a0c942b1ef3b26d67faf35b427
Paulo-Tonelotti <paulohenrique63664@gmail.com>
2023-03-25 20:16:59 -0300

    Revert "Att impostos"
    This reverts commit 88f2f37de842ba7c2e0cc29a4bd9bd64c0d0c578.

--- src/executavel/PrincipalOrcamento.java
+++ src/executavel/PrincipalOrcamento.java
@@ -3,11 +3,9 @@ package executavel;
 import descontos.CalculaDesconto;
 import impostos.COFINS;
 import impostos.ICMS;
-import impostos.IHIT;
 import impostos.ISS;
 import impostos.Imposto;
 import impostos.PIS;
-import impostos.TemplateDeImpostoCondicional;
 import model.Item;
 import model.Orcamento;
 
@@ -18,7 +16,7 @@ public class PrincipalOrcamento {
 		orcamento.adicionaItem(new Item("Caneta", 200));
 		orcamento.adicionaItem(new Item("Borracha", 100));
 		orcamento.adicionaItem(new Item("Lápis", 300));
-		orcamento.adicionaItem(new Item("Caneta", 200));
+		orcamento.adicionaItem(new Item("Caderno", 200));
 		orcamento.adicionaItem(new Item("Tesoura", 300));
 		orcamento.adicionaItem(new Item("Régua", 300));
 		
@@ -26,14 +24,12 @@ public class PrincipalOrcamento {
 		double imposto =  impostoEncadeado.calcular(orcamento);
 		
 		double desconto = new CalculaDesconto().calculaDesc(orcamento);
-		TemplateDeImpostoCondicional template = new IHIT();
-		double templateCalculado = template.calcular(orcamento);
+		
 		
 		System.out.printf("VALOR ORÇAMENTO: %.2f\n", orcamento.valorTotal());
 		System.out.println("---------------------------");
 		System.out.printf("IMPOSTOS: %.2f\n", imposto);
 		System.out.printf("DESCONTO: %.2f\n" , desconto);
-		System.out.printf("IMPOSTO TEMPLATE: %.2f\n", templateCalculado);
 		System.out.println("---------------------------");
 	}
 }
--- src/model/Item.java
+++ src/model/Item.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Objects;
-
 public class Item {
 	
 	String descricao;
@@ -23,23 +21,6 @@ public class Item {
 	}
 	public void setValor(double valor) {
 		this.valor = valor;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(descricao);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Item other = (Item) obj;
-		return Objects.equals(descricao, other.descricao);
 	}
 	
 	
